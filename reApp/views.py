649257cae78ce847e4583f296a66fd895005789b
Andrew Ryan <aryan17@mail.csuchico.edu>
2017-07-14 13:17:24 -0700

    fixed static files, layout almost done

---
+++ reApp/views.py
@@ -0,0 +1,25 @@
+# from django.shortcuts import render
+
+# Create your views here.
+
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import authenticate, login, logout, get_user_model
+from django.contrib.auth.decorators import login_required
+from realestateSite import settings
+
+from .models import *
+
+
+def index(request):
+    context = {
+        'title':"Home",
+        }
+    return render(request,'home.html',context)
+
+def home(request):
+    context = {
+        'title':'Home',
+    }
+    return render(request, 'home.html', context)
