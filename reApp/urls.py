3ee9aace15cb62300404bc86e1aeb781e00906ff
Andrew Ryan <aryan17@mail.csuchico.edu>
2017-07-21 09:41:58 -0700

    finished initial development

--- reApp/urls.py
+++ reApp/urls.py
@@ -4,7 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    # url(r'^email/$', views.email, name='email'),
-    # url(r'^home/$', views.home, name='home'),
     url(r'^success/$', views.success, name='success'),
 ]
